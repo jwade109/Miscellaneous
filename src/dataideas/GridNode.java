0c342bb4288bda40502c655bd18c562ae8337295
willm97 <willm97@vt.edu>
2016-11-19 21:36:22 -0500

    Finished GridNode
    I put in the last methods in GridNode to do the things. They will get
    the things done.

--- src/dataideas/GridNode.java
+++ src/dataideas/GridNode.java
@@ -37,9 +37,83 @@ public class GridNode
     
     /**
      * Gets the number of dimensions this GridNode exists in.
+     * @return The dimensions of this GridNode.
      */
     public int getDimensions()
     {
         return dims;
+    }
+    
+    /**
+     * Gets the neighbors of this GridNode.
+     * @return  The neighborhood of this GridNode.
+     */
+    public GridNode[] getNeighbors()
+    {
+        return neighbors;
+    }
+    
+    /**
+     * Gets a neighbor of this GridNode in a specific direction.
+     * @param vector    The direction to add the GridNode.
+     * @return          One neighbor of this GridNode.
+     */
+    public GridNode getNeighbor(int[] vector)
+    {
+        return neighbors[this.vectorToPosition(vector)];
+    }
+    
+    /**
+     * Sets the neighborhood of this GridNode.
+     * @param neighbors     The new neighbors of this GridNode.
+     * @throws IllegalArgumentException
+     */
+    public void setNeighborhood(GridNode[] neighbors)
+    {
+        if (neighbors == null
+            || this.neighbors.length != neighbors.length)
+        {
+            throw new IllegalArgumentException();
+        }
+        this.neighbors = neighbors;
+    }
+    
+    /**
+     * Sets a specific neighbor based on a basis vector for direction.
+     * @param neighbor  A GridNode to set as a neighbor.
+     * @param vector    The direction vector to add the neighbor
+     * @throws IllegalArgumentException
+     */
+    public void setNeighbor(GridNode neighbor, int[] vector)
+    {
+        if (neighbor == null
+            || vector.length != neighbors.length)
+        {
+            throw new IllegalArgumentException();
+        }
+        neighbors[this.vectorToPosition(vector)] = neighbor;
+    }
+    
+    /**
+     * Turns a vector into a value in the array.
+     * @param vector    A vector to process into a position.
+     * @return          The position to return.
+     * @throws IllegalArgumentException
+     */
+    private int vectorToPosition(int[] vector)
+    {
+        if (vector == null)
+        {
+            throw new IllegalArgumentException();
+        }
+        int position = 0;
+        for (int i = 0; i < vector.length; i++)
+        {
+            if (vector[i] == 1)
+            {
+                position += (int) Math.pow(2, i) + 0.01;
+            }
+        }
+        return position;
     }
 }
