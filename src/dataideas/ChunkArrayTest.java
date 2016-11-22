99d41c6fc2cd898ce95a92cb32983e45882edea3
willm97 <willm97@vt.edu>
2016-11-21 22:32:51 -0500

    Finished tests for ChunkArray... it seems
    All good for now.

--- src/dataideas/ChunkArrayTest.java
+++ src/dataideas/ChunkArrayTest.java
@@ -18,6 +18,7 @@ public class ChunkArrayTest
     @Before
     public void setUp()
     {
+        array = new ChunkArray<Integer>();
         array.setEntry(101, 0, 0);
         array.setEntry(102, 1, 1);
         array.setEntry(103, 49, 49);
@@ -29,6 +30,7 @@ public class ChunkArrayTest
         // Now a non empty chunk is after them in the list.
         array.setEntry(106, 50, 0);
         array.setEntry(107, -50, 0);
+        array.getEntry(10000, 10000);
         array.setEntry(108, 0, -50);
     }
 
@@ -38,6 +40,7 @@ public class ChunkArrayTest
     @Test
     public void testGetEntry()
     {
+        // Normal cases.
         assertEquals(101, (int) array.getEntry(0, 0));
         assertEquals(102, (int) array.getEntry(1, 1));
         assertEquals(103, (int) array.getEntry(49, 49));
@@ -47,7 +50,11 @@ public class ChunkArrayTest
         assertEquals(107, (int) array.getEntry(-50, 0));
         assertEquals(108, (int) array.getEntry(0, -50));
         
+        // No value in a slot.
         assertNull(array.getEntry(-5, -10));
+        
+        // Testing for the linkedlist length bug.
+        assertNull(array.getEntry(502, 502));
     }
     
     /**
@@ -65,7 +72,13 @@ public class ChunkArrayTest
         assertEquals(100, (int) array.getEntry(-100, -100));
     }
     
-    
-    
-
+    /**
+     * Tests the clear() method.
+     */
+    public void testClear()
+    {
+        array.clear();
+        assertNull(array.getEntry(0, 0));
+        assertNull(array.getEntry(1, 1));
+    }
 }
