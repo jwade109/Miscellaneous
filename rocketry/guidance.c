28746ff5926f7827a6a034f80b3cee99c22045f2
Wade Foster <jwade109@vt.edu>
2017-03-12 13:26:41 -0400

    Removed all that allegro garbage, added comments and anti-overshoot measures

--- rocketry/guidance.c
+++ rocketry/guidance.c
@@ -4,8 +4,6 @@
 #include <stdlib.h>
 #include <time.h>
 #include <string.h>
-#include <allegro5/allegro.h>
-#include <allegro5/allegro_primitives.h>
 
 int main(int argc, char** argv)
 {
@@ -15,11 +13,15 @@ int main(int argc, char** argv)
     int noPrint = 0;
     int compact = 0;
     int graphical = 0;
-    int window = 0;
 
-    double yb = 3100, vb = 210, kl = 0.005, kh = 0.012, m = 5, target = 3800;
+    double yb = 3100, vb = 210, vmin = 2, kl = 0.005, kh = 0.012, m = 5, target = 3800;
     double dt = 0.05;
-    double P = 1, I = 0, D = 0;    
+    double P = 1, I = 0, D = 0;
+
+    /*
+     * this god-awful for loop processes all the possible arguments for
+     * command line invokation
+     */
 
     for (int i = 1; i < argc; i++)
     {
@@ -30,6 +32,10 @@ int main(int argc, char** argv)
         else if (!strcmp(argv[i], "vb"))
         {
             sscanf(argv[i+1], "%lf", &vb);
+        }
+        else if (!strcmp(argv[i], "vmin"))
+        {
+            sscanf(argv[i+1], "%lf", &vmin);
         }
         else if (!strcmp(argv[i], "kl"))
         {
@@ -77,30 +83,30 @@ int main(int argc, char** argv)
         {
             graphical = 1;
         }
-        else if (!strcmp(argv[i], "window"))
-        {
-            window = 1;
-        }
-    }
-    
-    ALLEGRO_DISPLAY *display = NULL;
-    if (window)
-    {
-        al_init();
-        al_init_primitives_addon();
-        display = al_create_display(1000, 1000);
-        al_draw_filled_triangle(500,500,700,500,500,700, al_map_rgb(0,255,0));
-        al_flip_display();
     }
 
+    /*
+     * initialize the step velocity to burnout velocity, the step height to burnout height,
+     * and the step drag gain to the passive drag gain.
+     */
     double vi = vb;
     double yi = yb;
     double ki = kl;
 
+    /*
+     * calculate the range of achievable target altitudes given
+     * the vehicle's current trajectory and characteristics
+     */
     double ld_ta = t_a(vb, m, kl);
     double ld_alt = alt(yb, vb, m, kl, ld_ta);
     double hd_ta = t_a(vb, m, kh);
     double hd_alt = alt(yb, vb, m, kh, hd_ta);
+
+    /*
+     * header information is printed here. this includes target, burnout height, burnout
+     * velocity, low drag constant, high drag constant, vehicle mass, low-drag altitude (100p),
+     * high-drag altitude (100a), the simulation timestep, and the PID tuning gains.
+     */
 
     if (!noPrint)
     {
@@ -123,7 +129,7 @@ int main(int argc, char** argv)
         if (!graphical)
         {
             printf("(All values are displayed in SI units.)\n");
-            printf("Time\t\tDest. Alt.\tFlap State\tAltitude\t\tVelocity\tAcceleration\n");
+            printf("Time\t\tDest. Alt.\tFlap State\tAltitude\tVelocity\tAcceleration\n");
         }
         else
         {
@@ -131,47 +137,118 @@ int main(int argc, char** argv)
             printf("Time\tDest. Alt.\tVelocity\tError\t- v +\n");
         }
     }
-
-    if (!noWait) wait(2);
-    double t = 0;
-
+    
+    double t = 0; // keeps track of time since burnout
+    double max_error = 0; // for graphical display. computationally unnecessary
     Controller flaps = pid_init(P,I,D,-1);
-
-    double max_error = 0;
-    while(vi > 0)
+    if (!noWait) wait(2);
+    
+    /*
+     * updates every dt seconds (as many times per second as possible, ideally)
+     * and runs calculations to attenuate the flaps and reach apogee, until
+     * velocity reaches some threshold minimum (apogee is reached).
+     */
+    while(vi > vmin)
     {
+        /*
+         * at this point in the loop the experimentally measured values
+         * should be determined. for the algorithm to work, only instantaneous
+         * altitude and velocity must be known, within a reasonable degree of
+         * certainty. if more time must be taken to minimize the variance of the
+         * measurements, that's fine -- this is fairly robust to low polling
+         * rates, though I think at least 5 Hz is necessary
+         */
+         
+        // TODO: calculate real vi (step velocity) and yi (step altitude)
+    
+        /*
+         * this block calculates the time to apogee and the height
+         * of apogee if the rest of the flight is flown with passive
+         * drag. it appraises the state of the trajectory to predict
+         * if the target will be reached without further intervention.
+         */
         double ta_passive = t_a(vi, m, kl);
         double ya_passive = alt(yi, vi, m, kl, ta_passive);
 
+        /* computationally unnecessary. used for graphical display */
         if (ya_passive - target > max_error)
             max_error = ya_passive - target;
-
+        
+        /*
+         * calculates the error between the predicted altitude
+         * and the target; only checks for positive error, since
+         * there's nothing we can do about negative error and it
+         * really shouldn't ever happen
+         */ 
         double error;
-        if (ya_passive > (target + dt * 15))
+        if (ya_passive > target)
             error = -pid_seek(&flaps, ya_passive, target, dt);
         else
             error = 0;
         
+        /*
+         * decides to engage the flaps or not.
+         * if error is above a certain threshold, engage flaps.
+         */
+        double ya_passive_next = 0;
+        int flap_state = 0;
         if (error > 1)
-            ki = kh;
+        {
+            /*
+             * calculates the next step's predicted altitude to confirm
+             * that high drag should be chosen for this step.
+             * this is to avoid overshooting, since more drag
+             * can always be applied later.
+             */
+            
+            double yi_next = alt(yi, vi, m, kh, dt);
+            double vi_next = vel(vi, m, kh, dt);
+            double ta_passive_next = t_a(vi_next, m, kl);
+            ya_passive_next = alt(yi_next, vi_next, m, kl, ta_passive_next);
+            if (ya_passive_next > target)
+            {
+                ki = kh;
+                flap_state = 1;
+            }
+            else
+            {
+                ki = kl;
+                flap_state = 0;
+            }
+        }
         else
+        {
             ki = kl;
+            flap_state = 0;
+        }
 
+        /*
+         * this block is unnecessary for the final algorithm - in this
+         * simulation, the next step's instantaneous altitude and velocity
+         * are calcuted using the ideal equations. in the field, they will
+         * be measured experimentally.
+         */
         yi = alt(yi, vi, m, ki, dt);
         vi = vel(vi, m, ki, dt);
+        
+        /* unnecessary to calculate acceleration but useful to know. */
         double ai = accel(vi, m, ki, dt);
         
+        /* prints out useful data */
         if (!noPrint)
         {
+            /* prints 6 columns of data if graphical is disabled */
             if (!graphical)
             {
                 printf("\r%g", t);
                 printf("\r\t\t%g", ya_passive);
-                printf("\r\t\t\t\t%d", (int) error);
+                printf("\r\t\t\t\t%d    %d", flap_state, (int) error);
                 printf("\r\t\t\t\t\t\t%g", yi);
-                printf("\r\t\t\t\t\t\t\t\t\t\t%g", vi);
-                printf("\r\t\t\t\t\t\t\t\t\t\t\t\t%g", ai);
+                printf("\r\t\t\t\t\t\t\t\t%g", vi);
+                printf("\r\t\t\t\t\t\t\t\t\t\t%g", ai);
+                printf("\r\t\t\t\t\t\t\t\t\t\t\t\t%g", ya_passive_next);
             }
+            /* prints 3 columns of data and an error bar if graphical is enabled */
             else
             {
                 printf("%g", t);
@@ -192,17 +269,19 @@ int main(int argc, char** argv)
             }
             fflush(stdout);
         }
-        if (!noWait) al_rest(dt);
-        if (!noPrint) 
+        if (!noWait) wait(dt*0.9); // wait dt seconds
+        
+        /* erases the old line if compact is enabled, starts a new line if not */
+        if (!noPrint)
         {
-            if (compact && vi > 0)
+            if (compact && vi > vmin)
             {
                 printf("\r                                                    ");
                 printf("                                                         \r");
             }
             if (!compact) printf("\n");
         }
-        t += dt;
+        t += dt; // advance t by dt
     }
 
     double elapsed = ((double)(clock() - start))/CLOCKS_PER_SEC;
@@ -210,9 +289,6 @@ int main(int argc, char** argv)
     if (!noPrint) printf("- - - - - - - - - -\nApogee.\n");
     printf("Time elapsed: %g seconds (%g microseconds)\n", elapsed, 1000000*elapsed);
     if (!noPrint) printf("\n");
-
-    if (window) while(1);
-    al_destroy_display(display);
 
     return 0;
 }
