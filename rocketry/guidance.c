1e4d86f897580cfca53cc7c6c282615bc7c068b3
Wade Foster <jwade109@vt.edu>
2017-03-14 19:01:25 -0400

    Removed noise stuff because it's hard to characterize the error as gaussian

--- rocketry/guidance.c
+++ rocketry/guidance.c
@@ -5,20 +5,6 @@
 #include <math.h>
 #include <string.h>
 
-double gaussianNoise(double mu, double sigma)
-{
-	double u1, u2;
-	do
-	{
-	   u1 = rand() * (1.0 / RAND_MAX);
-	   u2 = rand() * (1.0 / RAND_MAX);
-	}
-	while (u1 <= 0.0000001);
-
-	return mu + sigma * sqrt(-2.0 * log(u1)) * cos(2 * M_PI * u2);
-	wait(0.1);
-}
-
 int main(int argc, char** argv)
 {
     /*
@@ -39,7 +25,6 @@ int main(int argc, char** argv)
 
     double yb = 3100, vb = 210, vmin = 8, kl = 0.005, kh = 0.012, m = 5, target = 3800;
     double dt = 0.05;
-    double alt_sigma = 0, vel_sigma = 0;
 
     /*
      * this god-awful for loop processes all the possible arguments for
@@ -97,11 +82,6 @@ int main(int argc, char** argv)
         {
             graphical = 1;
         }
-        else if (!strcmp(argv[i], "noisy"))
-        {
-            sscanf(argv[i+1], "%lf", &alt_sigma);
-            sscanf(argv[i+2], "%lf", &vel_sigma);
-        }
     }
 
     /*
@@ -120,14 +100,6 @@ int main(int argc, char** argv)
     double ld_alt = alt(yb, vb, m, kl, ld_ta);
     double hd_ta = t_a(vb, m, kh);
     double hd_alt = alt(yb, vb, m, kh, hd_ta);
-
-
-    if (noPrint)
-    {
-        double data[20] = {9, 2, 5, 4, 12, 7, 8, 11, 9, 3, 7, 4, 12, 5, 4, 10, 9, 6, 9, 4};
-        int size = 20;
-        printf("%g\n", sigma(data, size));
-    }
     
     /*
      * header information is printed here. this includes target, burnout height, burnout
@@ -191,30 +163,11 @@ int main(int argc, char** argv)
          * simulation, the next step's instantaneous altitude and velocity
          * are calcuted using the ideal equations. in the field, they will
          * be measured experimentally.
-         */        
+         */
         if (!firstStep)
         {
             yi = alt(yi, vi, m, ki, dt);
             vi = vel(vi, m, ki, dt);
-        }
-        
-        /*
-         * this block simulates sensor noise which follows a gaussian distribution
-         * around the expected value, which perfectly follows the ideal equations.
-         * this is unnecessary for the final program, since sensor noise is a given
-         */
-        double yi_measured = yi;
-        double vi_measured = vi;
-        
-        if (alt_sigma > 0 || vel_sigma > 0)
-        {
-            yi_measured = gaussianNoise(yi, alt_sigma);
-            vi_measured = gaussianNoise(vi, vel_sigma);
-        }
-        else
-        {
-            yi_measured = yi;
-            vi_measured = vi;
         }
         
         /*
@@ -223,14 +176,14 @@ int main(int argc, char** argv)
          * overshooting, but is likely to undershoot if the polling rate is too low
          */
         
-        /* 100% accurate data about the current step, for the simulation */
+        /* data about the current step */
         double ta_passive = t_a(vi, m, kl);
         double ya_passive = alt(yi, vi, m, kl, ta_passive);
         double error = ya_passive - target;
         
-        /* noisy data about the next step, if the flaps are deployed */
-        double yi_next = alt(yi_measured, vi_measured, m, kh, dt);
-        double vi_next = vel(vi_measured, m, kh, dt);
+        /* data about the next step, if the flaps are deployed */
+        double yi_next = alt(yi, vi, m, kh, dt);
+        double vi_next = vel(vi, m, kh, dt);
         double ta_passive_next = t_a(vi_next, m, kl);
         double ya_passive_next = alt(yi_next, vi_next, m, kl, ta_passive_next);
         
@@ -265,8 +218,8 @@ int main(int argc, char** argv)
                 printf("\r\t\t%g", ya_passive);
                 printf("\r\t\t\t\t%g", ya_passive_next);
                 printf("\r\t\t\t\t\t\t%s (%d)", flap_state, (int) error);
-                printf("\r\t\t\t\t\t\t\t\t%g", yi_measured - yi);
-                printf("\r\t\t\t\t\t\t\t\t\t\t%g", vi_measured);
+                printf("\r\t\t\t\t\t\t\t\t%g", yi);
+                printf("\r\t\t\t\t\t\t\t\t\t\t%g", vi);
                 // printf("\r\t\t\t\t\t\t\t\t\t\t\t\t%g", ai);
             }
             /* prints 3 columns of data and an error bar if graphical is enabled */
@@ -312,7 +265,7 @@ int main(int argc, char** argv)
     if (compact && !noPrint) printf("\n");
     if (!noPrint) printf("- - - - - - - - - -\nApogee. Final error: ");
     printf("%d meters\n", (int) min_error);
-    printf("Time elapsed: %g seconds (%g microseconds)\n", elapsed, 1000000*elapsed);
+    if (noPrint) printf("Time elapsed: %g seconds (%g microseconds)\n", elapsed, 1000000*elapsed);
     if (!noPrint) printf("\n");
 
     return 0;
