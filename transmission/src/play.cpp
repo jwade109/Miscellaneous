22669b3557b109b143d563d19f2a6c0236332c11
Wade Foster <jwade109@vt.edu>
2019-07-01 03:18:21 +0000

    Add log duration to play

--- transmission/src/play.cpp
+++ transmission/src/play.cpp
@@ -1,6 +1,7 @@
 
 #include <transmission/packet.hpp>
 #include <transmission/server.hpp>
+#include <transmission/util.hpp>
 
 #include <algorithm>
 
@@ -35,18 +36,29 @@ int main(int argc, char **argv)
         serv.call(p);
     }
 
-    auto real_start = std::chrono::system_clock::now();
+    using namespace std::chrono;
+
+    auto real_start = system_clock::now();
     auto sim_start = packets[0].time();
+    auto sim_duration = packets[packets.size() - 1].time() - sim_start;
+
+    std::cout << "Log begins at " << rvt::dateString(sim_start)
+        << ", and spans " << duration<double, std::ratio<60>>
+        {sim_duration}.count() << " minutes." << std::endl;
 
     for (auto p : serv.history)
     {
         auto sim_time = p.time();
         auto real_time = std::chrono::system_clock::now();
         while (real_time - real_start < sim_time - sim_start)
+        {
             real_time = std::chrono::system_clock::now();
+        }
 
         std::cout << pprintf(rvt::packet::print_format, p) << std::endl;
     }
+
+    std::cout << "Done." << std::endl;
 
     return 0;
 }
