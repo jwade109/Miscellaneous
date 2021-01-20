b3572bf6f10a6fc68880736de632e4df44c01408
Wade Foster <foster@torc.ai>
2021-01-20 17:55:28 -0500

    Support multiple cistercian digits

--- cistercian_numerals/cistercian.py
+++ cistercian_numerals/cistercian.py
@@ -1,8 +1,10 @@
 #! /usr/bin/env python2.7
+# -*- coding: utf-8 -*-
 
 import argparse
 from math import floor, ceil, log
 from os import popen
+import sys
 
 base = 10000 # base of cistercian numeral system
 numeral_width = 11 # must be odd
@@ -19,21 +21,22 @@ def print_numeral(numeral):
 def print_numerals(numerals):
     if not numerals:
         return
+    if not numerals[0]:
+        return
     _, columns = popen('stty size', 'r').read().split()
     columns = int(columns)
-    effective_width = 20
-    num_to_print = min(len(numerals), int(floor(columns/effective_width)))
- 
+    effective_width = len(numerals[0][0]) * 2 + 3
+    num_to_print = max(1, min(len(numerals), int(floor(columns/effective_width))))
+
     for r in range(0, len(numerals[0])):
         for numeral in numerals[:num_to_print]:
             row = numeral[r]
-            print("{}  ".format("".join(row))),
+            sys.stdout.write("  {}".format("".join([x*2 for x in row])))
         print("")
 
     if num_to_print < len(numerals):
         print("")
         print_numerals(numerals[num_to_print:])
-
 
 
 def empty_numeral(width):
@@ -47,7 +50,7 @@ def base_numeral(width):
     numeral = list()
     half_width = int(floor(width/2.0))
     for i in range(0, width):
-        numeral.append([" "]*half_width + ["#"] + [" "]*half_width)
+        numeral.append([" "]*half_width + ["█"] + [" "]*half_width)
     return numeral
 
 
@@ -57,20 +60,20 @@ def segment(width, value):
     if value == 0:
         return base_numeral(width)
     elif value == 1:
-        numeral[0][-half_width:] = ["#"]*half_width
+        numeral[0][-half_width:] = ["█"]*half_width
     elif value == 2:
-        numeral[half_width-1][-half_width:] = ["#"]*half_width
+        numeral[half_width-1][-half_width:] = ["█"]*half_width
     elif value == 3:
         for i in range(0, half_width):
-            numeral[i][half_width+1+i] = "#"
+            numeral[i][half_width+1+i] = "█"
     elif value == 4:
         for i in range(0, half_width):
-            numeral[i][width-i-1] = "#"
+            numeral[i][width-i-1] = "█"
     elif value == 5:
         return overlay(segment(width, 4), segment(width, 1))
     elif value == 6:
-        for i in range(0, half_width-1):
-            numeral[i][width-1] = "#"
+        for i in range(0, half_width):
+            numeral[i][width-1] = "█"
     elif value == 7:
         return overlay(segment(width, 6), segment(width, 1))
     elif value == 8:
@@ -128,13 +131,11 @@ def decompose(num):
     return l[::-1]
 
 
-
 def main():
-    parser = argparse.ArgumentParser();
+    parser = argparse.ArgumentParser()
     parser.add_argument("number", type=int)
-    parser.add_argument("--width", "-w", type=int, default=9)
+    parser.add_argument("--width", "-w", type=int, default=7)
     args = parser.parse_args()
-    print(args)
 
     if args.width % 2 == 0 or args.width < 5:
         print("Width must be an odd number of at least 5.")
@@ -143,10 +144,10 @@ def main():
     num = args.number
     width = args.width
     digits = 1 if num == 0 else int(ceil(log(num, base)))
-    print("Digits = {}".format(digits))
-    print("decompose({}) = {}".format(num, decompose(num)))
+    # print("Digits = {}".format(digits))
+    # print("decompose({}) = {}".format(num, decompose(num)))
     print("")
-    print_numeral(segment(width, num))
+    print_numerals([segment(width, n) for n in decompose(num)])
     print("")
 
 
