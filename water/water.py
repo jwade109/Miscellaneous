1d032093bddba98a1ca81647a318dae30d851038
Wade Foster <foster@torc.ai>
2021-03-31 15:51:50 -0400

    Modifications to water.

--- water/water.py
+++ water/water.py
@@ -5,6 +5,7 @@ from math import sqrt, sin, exp
 from os import popen
 import sys
 import curses
+from curses.textpad import rectangle
 from time import sleep
 
 try:
@@ -48,24 +49,42 @@ def tochr(value):
     return " "
 
 
-def main(stdscr):
+def main(screen):
 
-    stdscr.clear()
+    screen.clear()
+    curses.curs_set(0)
+
+    message = None
+    if len(sys.argv) > 1:
+        message = sys.argv[1]
 
     t = 0.1
     try:
         while True:
             cols, rows = get_terminal_size()
+            text_r = rows//4
+            text_c = cols//4
             cols = cols//2
             for r in range(rows-1):
                 for c in range(cols):
-                    setval(stdscr, r, c, tochr(
-                        radial(r, c, t/2, 0.3, 200, -rows, -cols) + \
-                        radial(r, c, t/2, 0.3, 200, 2*rows,-1.9*cols) + \
-                        radial(r, c, t/1.5, 0.3, 80, 0.25*rows, 0.25*cols) * 1.0 + \
+                    setval(screen, r, c, tochr(
+                        radial(r, c, t/2, 0.3, cols*1.5, -rows, -cols) + \
+                        radial(r, c, t/2, 0.3, cols*1.5, 2*rows,-1.9*cols) + \
+                        radial(r, c, t/1.5, 0.3, 80, 0.25*rows, 0.25*cols) * 0.7 + \
                         radial(r, c, t/2.0, 0.6, 40, 0.70*rows, 0.60*cols) * 0.5 + \
                         radial(r, c, t*1.5, 0.8, 40, 0.24*rows, 0.75*cols) * 0.3))
-            stdscr.refresh()
+            if message:
+                screen.addstr(text_r - 1, text_c - 2, " "*(len(message) + 4))
+                screen.addstr(text_r    , text_c - 2, " "*(len(message) + 4))
+                screen.addstr(text_r + 1, text_c - 2, " "*(len(message) + 4))
+                screen.addstr(text_r, text_c, message)
+                ulr = text_r - 2
+                ulc = text_c - 3
+                lrr = text_r + 2
+                lrc = text_c + len(message) + 2
+                rectangle(screen, ulr, ulc, lrr, lrc)
+
+            screen.refresh()
             sleep(0.05)
             t += 0.2
     except KeyboardInterrupt:
